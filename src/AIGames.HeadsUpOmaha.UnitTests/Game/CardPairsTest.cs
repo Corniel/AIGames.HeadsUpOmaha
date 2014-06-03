c5c0ba6f95ba7fbd8e5e63c774332e8b48bda162
Corniel Nobel <cornieln@yahoo.com>
2014-06-03 12:46:43 +0200

    Added best and second (best) to card pairs, including UnitTest.

---
+++ src/AIGames.HeadsUpOmaha.UnitTests/Game/CardPairsTest.cs
@@ -0,0 +1,68 @@
+﻿using AIGames.HeadsUpOmaha.Game;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AIGames.HeadsUpOmaha.UnitTests.Game
+{
+	[TestClass]
+	public class CardPairsTest
+	{
+		[TestMethod]
+		public void Best_Empty_AreEqual()
+		{
+			var pairs = CardPairs.Create(Cards.Empty);
+
+			var act = pairs.Best;
+			var exp = 0;
+
+			Assert.AreEqual(exp, act);
+		}
+
+		[TestMethod]
+		public void Best_56KQ_AreEqual()
+		{
+			var cards = Cards.Parse("[5c,6c,Kh,Qc]");
+			var pairs = CardPairs.Create(cards);
+
+			var act = pairs.Best;
+			var exp = 13;
+
+			Assert.AreEqual(exp, act);
+		}
+
+		[TestMethod]
+		public void Best_56KK_AreEqual()
+		{
+			var cards = Cards.Parse("[5c,6c,Kh,Kc]");
+			var pairs = CardPairs.Create(cards);
+
+			var act = pairs.Best;
+			var exp = 13;
+
+			Assert.AreEqual(exp, act);
+		}
+
+		[TestMethod]
+		public void Second_56KQ_AreEqual()
+		{
+			var cards = Cards.Parse("[5c,6c,Kh,Qc]");
+			var pairs = CardPairs.Create(cards);
+
+			var act = pairs.Second;
+			var exp = 12;
+
+			Assert.AreEqual(exp, act);
+		}
+
+		[TestMethod]
+		public void Second_56KK_AreEqual()
+		{
+			var cards = Cards.Parse("[5c,6c,Kh,Kc]");
+			var pairs = CardPairs.Create(cards);
+
+			var act = pairs.Second;
+			var exp = 13;
+
+			Assert.AreEqual(exp, act);
+		}
+	}
+}
--- src/AIGames.HeadsUpOmaha/Game/CardPairs.cs
+++ src/AIGames.HeadsUpOmaha/Game/CardPairs.cs
@@ -1,22 +1,53 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace AIGames.HeadsUpOmaha.Game
 {
 	/// <summary>Represents pairs based on a set of cards.</summary>
+	[DebuggerDisplay("{DebugToString()}")]
 	public struct CardPairs
 	{
-		/// <summary>Represents an empty set of card pairs.</summary>
-		public static readonly CardPairs Empty = default(CardPairs);
-
 		private int[] lookup;
 
 		/// <summary>Gets the number of cards with the specified height.</summary>
 		/// <param name="height"></param>
 		public int this[int height] { get { return lookup[height]; } }
 
+		/// <summary>Get the best card.</summary>
+		public int Best { get { return GetBest(0); } }
+		/// <summary>Gets the second best card.</summary>
+		public int Second { get { return GetBest(1); } }
+		
+		/// <summary>Get the nth best card.</summary>
+		public int GetBest(int pos)
+		{
+			var p = 0;
+			for (var height = 14; height >= 0; height--)
+			{
+				p += lookup[height];
+				if (p > pos) { return height; }
+			}
+			return 0;
+		}
+
 		/// <summary>Gets the highest number of pairs.</summary>
 		public int Max { get { return lookup.Max(); } }
+
+		/// <summary>Represents a card as a debug string.</summary>
+		[System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+		private string DebugToString()
+		{
+			if (lookup == null || Max == 0) { return "Pairs: {empty}"; }
+			return String.Format(
+				"Pairs: Max: {0}, Best: {1}, Second: {2}",
+				this.Max,
+				Numbers[this.Best],
+				Numbers[this.Second]
+			);
+		}
+		private const string Numbers = " ?23456789TJQKA";
 
 		/// <summary>Creates card pairs based on set of cards.</summary>
 		/// <param name="cards">
