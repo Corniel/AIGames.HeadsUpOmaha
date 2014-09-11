c1a132dd57288794fe5032945792acc0290359b6
Corniel <mail@corniel.nl>
2014-09-11 12:54:17 +0200

    Missing comparer added.

---
+++ src/AIGames.HeadsUpOmaha.Arena/BotCompareType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIGames.HeadsUpOmaha.Arena
+{
+	public enum BotCompareType
+	{
+		Elo = 0,
+		Percentage = 1,
+	}
+}
---
+++ src/AIGames.HeadsUpOmaha.Arena/BotComparer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace AIGames.HeadsUpOmaha.Arena
+{
+	public class BotComparer : IComparer<Bot>
+	{
+		public BotComparer (BotCompareType compareType)
+		{
+			this.CompareType = compareType;
+		}
+		public BotCompareType CompareType { get; protected set; }
+
+		public int Compare(Bot x, Bot y)
+		{
+			switch (this.CompareType)
+			{
+				case BotCompareType.Percentage: return y.Score.CompareTo(x.Score);
+				case BotCompareType.Elo:
+				default: return y.Rating.CompareTo(x.Rating);
+			}
+		}
+	}
+}
